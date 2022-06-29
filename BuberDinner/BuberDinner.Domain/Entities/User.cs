e81831ca6c8f9b51c755bcd0f2f3c0afe5e15806
CyberMuffin <cybermuffinofficial@gmail.com>
2022-06-29 06:23:31 +0530

    new guid on user creation

--- BuberDinner/BuberDinner.Domain/Entities/User.cs
+++ BuberDinner/BuberDinner.Domain/Entities/User.cs
@@ -8,7 +8,7 @@ namespace BuberDinner.Domain.Entities;
 
 public class User
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
 
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
