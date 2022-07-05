cecfcd42239e2d458c661d29515487c50f14a6e2
CyberMuffin <cybermuffinofficial@gmail.com>
2022-07-05 13:58:47 +0530

    Introduced ApiController. Working. Only ErrorOr library being used

---
+++ BuberDinner/BuberDinner.Api/Common/Http/HttpContextItemKeys.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Common.Http;
+
+public class HttpContextItemKeys
+{
+    public const string Errors = "Errors";
+}
--- BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -2,13 +2,14 @@
 using BuberDinner.Contracts.Authentication;
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using BuberDinner.Domain.Common.Errors;
 
 namespace BuberDinner.Api.Controllers;
 
-[ApiController]
+
 [Route("auth")]
 //[ErrorHandlingFilter]
-public class AuthenticationController : ControllerBase
+public class AuthenticationController : ApiController
 {
 
     private readonly IAuthenticationService _authenticationService;
@@ -23,20 +24,23 @@ public class AuthenticationController : ControllerBase
     {
         ErrorOr<AuthenticationResult> authResult = _authenticationService.Register(request.FirstName, request.LastName, request.Email, request.Password);
 
-        return authResult.MatchFirst(authResult => Ok(MapAuthResult(authResult)), firstError => Problem(statusCode: StatusCodes.Status409Conflict, title: firstError.Description));
-        //     return authResult.Match(
-        //authResult => Ok(MapAuthResult(authResult)),
-        //_ => Problem(statusCode: StatusCodes.Status409Conflict, title: "asd")
-        // //errors => Problem(errors) // pass this to the method defined in the ApiController class
-        // );
+        //return authResult.MatchFirst(authResult => Ok(MapAuthResult(authResult)), firstError => Problem(statusCode: StatusCodes.Status409Conflict, title: firstError.Description));
+
+
+        return authResult.Match(authResult => Ok(MapAuthResult(authResult)), errors => Problem(errors)); // pass this to the method defined in the ApiController class
     }
 
     [HttpPost("login")]
     public IActionResult Login(LoginRequest request)
     {
         var authResult = _authenticationService.Login(request.Email, request.Password);
-        var response = new AuthenticationResponse(authResult.User.Id, authResult.User.FirstName, authResult.User.LastName, authResult.User.Email, authResult.Token);
-        return Ok(response);
+
+        if (authResult.IsError && authResult.FirstError == Errors.Authentication.InvalidCredentials)
+        {
+            return Problem(statusCode: StatusCodes.Status401Unauthorized, title: authResult.FirstError.Description);
+        }
+
+        return authResult.Match(authResult => Ok(MapAuthResult(authResult)), errors => Problem(errors)); // pass this to the method defined in the ApiController class
     }
 
     private static AuthenticationResponse MapAuthResult(AuthenticationResult authResult)
