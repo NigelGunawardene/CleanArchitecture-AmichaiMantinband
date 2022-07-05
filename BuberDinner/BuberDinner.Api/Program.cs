d300dcd8e1caa7690067b0be1a990c8e5d7a2ac9
CyberMuffin <cybermuffinofficial@gmail.com>
2022-07-05 11:32:55 +0530

    Error endpoint that uses custom error factory

--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,6 +1,4 @@
 ﻿using BuberDinner.Api.Errors;
-using BuberDinner.Api.Filters;
-using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 using Microsoft.AspNetCore.Diagnostics;
@@ -12,11 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-    //builder.Services.AddApplication();
-    //builder.Services.AddInfrastructure();
-
-    // disabling this as well (after middleware)
-    //builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>()); // use for config and dependency injection
 
     builder.Services.AddControllers();
 
@@ -25,8 +18,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
-    // commenting this out in order to not use the middleware approach for now
-    //app.UseMiddleware<ErrorHandlingMiddleware>();
 
     // 3rd approach to error handling - error endpoint
     app.UseExceptionHandler("/error"); // this is used in conjunction with the factory 
