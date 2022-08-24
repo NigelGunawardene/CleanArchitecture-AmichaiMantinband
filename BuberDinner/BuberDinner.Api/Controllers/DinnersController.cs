56a762abab55a56dc14284a3c565c2fb2bac4b87
CyberMuffin <cybermuffinofficial@gmail.com>
2022-08-24 07:47:21 +0530

    Only added authentication

---
+++ BuberDinner/BuberDinner.Api/Controllers/DinnersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+[Route("[controller]")]
+public class DinnersController : ApiController
+{
+    [HttpGet]
+    public IActionResult ListDinners()
+    {
+        return Ok(Array.Empty<string>());
+    }
+}
--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -41,6 +41,7 @@ var app = builder.Build();
     //});
 
     app.UseHttpsRedirection();
+    app.UseAuthentication();
     app.MapControllers();
 
     app.Run();
